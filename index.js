9469d372ad3ccc50aaaf6280b482a6e9c18412a9
Maik Hummel <maik@neohelden.com>
2020-01-14 15:54:25 +0100

    Expose NPQ

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const task = require('./lib/task')
+
 module.exports = {
   log: require('./lib/log'),
-  task: require('./lib/task'),
+  task: task,
   i18n: require('./lib/i18n'),
   got: require('./lib/got'),
   sentry: require('./lib/sentry'),
+  npq: task.npq,
   mustache: require('mustache')
 }
